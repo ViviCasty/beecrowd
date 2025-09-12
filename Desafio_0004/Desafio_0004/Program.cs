7cbbc1c91f9c653fb242f9437da365bfbe357373
ViviCasty <ichpendragon@gmail.com>
2025-09-12 18:41:07 -0300

    ADICIONANDO MAIS DESAFIOS RESOLVIDOS

---
+++ Desafio_0004/Desafio_0004/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int soma = a + b;
+
+        Console.WriteLine("SOMA = "+ soma);
+        Console.WriteLine();
+    }
+
+}
---
+++ Desafio_0006/Desafio_0006/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+        Console.WriteLine($"MEDIA = {MEDIA.ToString("F5", CultureInfo.InvariantCulture)}");
+    }
+}
