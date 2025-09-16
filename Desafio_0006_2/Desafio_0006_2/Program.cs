86e0b49fdb195f5a447b6248151e1745d048f89b
ViviCasty <ichpendragon@gmail.com>
2025-09-15 23:51:37 -0300

    desafio 0006

---
+++ Desafio_0006_2/Desafio_0006_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double resultado = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+        Console.WriteLine("MEDIA = " + resultado.ToString("F1", CultureInfo.InvariantCulture));
+
+    }
+
+}
