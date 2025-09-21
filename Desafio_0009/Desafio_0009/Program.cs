cbe8a7ffb93ce65da1ce61bb02ae5267878dd108
ViviCasty <ichpendragon@gmail.com>
2025-09-21 16:34:33 -0300

    dESAFIO_0009

---
+++ Desafio_0009/Desafio_0009/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string Nome = Console.ReadLine();
+        double SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double total = SalarioFixo + (vendas * 0.15);
+
+        Console.WriteLine("TOTAL = R$ " + total.ToString("f2", CultureInfo.InvariantCulture));
+
+    }
+
+}
