dc33e18ca41d6ec46d4f66e1f6ff639c83345dcb
ViviCasty <ichpendragon@gmail.com>
2025-09-21 16:24:15 -0300

    Desafio_0008

---
+++ DESAFIO_0008/DESAFIO_0008/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int num = int.Parse(Console.ReadLine());
+        int horas = int.Parse(Console.ReadLine());
+        double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        double salario = horas * valor;
+
+        Console.WriteLine("NUMBER = " + num);
+        Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+
+    }
+
+}
