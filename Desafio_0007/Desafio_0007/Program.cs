8746752090b9699fa6c6192e72ef5d21039040ab
ViviCasty <ichpendragon@gmail.com>
2025-09-21 16:17:24 -0300

    DESAFIO 0007

---
+++ Desafio_0007/Desafio_0007/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+
+        int dif = (a * b - c * d);
+
+        Console.WriteLine("DIFERENÇA = " + dif);
+
+
+    }
+
+}
