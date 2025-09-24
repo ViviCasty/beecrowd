90294d67632509d94fd6ef4f8b860ab8fff5e2ca
ViviCasty <ichpendragon@gmail.com>
2025-09-24 11:16:56 -0300

    Desafio 1011

---
+++ Desafio_1011/Desafio_1011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double resultado = (4.0 / 3) * 3.14159 * Math.Pow(raio, 3);
+
+        Console.WriteLine("VOLUME = " + resultado.ToString("F3", CultureInfo.InvariantCulture));
+
+    }
+
+}
