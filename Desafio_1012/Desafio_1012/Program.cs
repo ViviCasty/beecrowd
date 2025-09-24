d8345e58441a92f60d2740ed76d8d3321d852a26
ViviCasty <ichpendragon@gmail.com>
2025-09-24 11:48:31 -0300

    Adicionar projeto Desafio_1012 com cálculo de áreas
    Adicionado o arquivo de solução `Desafio_1012.sln` e o arquivo de projeto `Desafio_1012.csproj`, configurando o uso do SDK do .NET 8.0.
    
    Implementado o arquivo `Program.cs`, que lê três valores e calcula a área de triângulo, círculo, trapézio, quadrado e retângulo, exibindo os resultados formatados com três casas decimais.

---
+++ Desafio_1012/Desafio_1012/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double[] valores = Console.ReadLine().Split(' ').Select(x => double.Parse(x, CultureInfo.InvariantCulture)).ToArray();
+
+        double A = valores[0];
+        double B = valores[1];
+        double C = valores[2];
+
+        double triangulo = (A * C) / 2;
+        double circulo = 3.14159 * Math.Pow(C, 2);
+        double trapezio = ((A + B) * C) / 2;
+        double quadrado = B * B;
+        double retangulo = A * B;
+
+        Console.WriteLine($"TRIANGULO: {triangulo.ToString("F3", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"CIRCULO: {circulo.ToString("F3", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"TRAPEZIO: {trapezio.ToString("F3",CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"QUADRADO: {quadrado.ToString("F3",CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"RETANGULO: {retangulo.ToString("F3", CultureInfo.InvariantCulture)}");
+
+
+
+
+    }
+
+}
