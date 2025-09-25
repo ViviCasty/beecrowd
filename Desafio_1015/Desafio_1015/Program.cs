9d9907af2d0817039205b721d8c9b7b68d368af9
ViviCasty <ichpendragon@gmail.com>
2025-09-25 14:54:42 -0300

    Adicionar projeto Desafio_1015 com cálculo de distância
    Criação do arquivo de solução `Desafio_1015.sln` e do arquivo de projeto `Desafio_1015.csproj`, configurando o uso do SDK do .NET 8.0.
    
    Implementação da classe `URI` no arquivo `Program.cs`, que lê duas coordenadas, calcula a distância entre elas e imprime o resultado formatado. Incluído método auxiliar `ToDouble` para conversão de strings em números de ponto flutuante.

---
+++ Desafio_1015/Desafio_1015/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] p1 = Console.ReadLine().Split(' ');
+        string[] p2 = Console.ReadLine().Split(' ');
+
+        double[] ponto1 = new double[2];
+        double[] ponto2 = new double[2];
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            ponto1[i] = ToDouble(p1[i]);
+            ponto2[i] = ToDouble(p2[i]);
+        }
+
+        double distancia = Math.Sqrt(Math.Pow(ponto2[0] - ponto1[0], 2) + Math.Pow(ponto2[1] - ponto1[1], 2));
+
+        Console.WriteLine(distancia.ToString("f4", CultureInfo.InvariantCulture));
+
+
+
+
+    }
+
+    static double ToDouble(string valor)
+    {
+        return double.Parse(valor, CultureInfo.InvariantCulture);
+    }
+
+}
