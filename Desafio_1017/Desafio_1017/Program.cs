f1b04eb2e24710b025fa017a8057b53219693bae
ViviCasty <ichpendragon@gmail.com>
2025-10-01 00:23:54 -0300

    Adicionar projeto Desafio 1017 com cálculo de combustível
    Criação do arquivo de solução `Desafio_1017.sln` com configurações do Visual Studio.
    Adição do arquivo de projeto `Desafio_1017.csproj` utilizando o SDK do .NET 8.0, definindo o tipo de saída como executável e habilitando usings implícitos e verificação de nulidade.
    Inclusão do arquivo `Program.cs` com a classe `URI`, que lê tempo e velocidade, calcula a quantidade de litros de combustível necessária e imprime o resultado formatado com três casas decimais.

---
+++ Desafio_1017/Desafio_1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int tempo =  int.Parse(Console.ReadLine());
+
+        int velocidade = int.Parse(Console.ReadLine());
+
+        double litros = (tempo * velocidade) / 12.0;
+
+        Console.WriteLine(litros.ToString("f3", CultureInfo.InvariantCulture));
+
+    }
+
+}
