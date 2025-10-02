d37cfcff02d031e88c5f2f30af45bdf4fd3bebcc
ViviCasty <ichpendragon@gmail.com>
2025-10-02 11:50:06 -0300

    Adicionar projeto Desafio_1020 com lógica de idade
    Criação do arquivo de solução `Desafio_1020.sln` e do arquivo de projeto `Desafio_1020.csproj`, configurando o uso do SDK do .NET 8.0. Implementação da classe `URI` em `Program.cs`, que calcula e exibe a idade em anos, meses e dias a partir da entrada do usuário.

---
+++ Desafio_1020/Desafio_1020/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int idade = int.Parse(Console.ReadLine());
+
+
+
+        int anos = idade / 365;
+
+        int restoAnos = idade % 365;
+
+        int meses = restoAnos / 30;
+
+        int dias = restoAnos % 30;
+
+        Console.WriteLine($"{anos} ano(s)");
+        Console.WriteLine($"{meses} mes(es)");
+        Console.WriteLine($"{dias} dia(s)");
+
+
+    }
+
+}
