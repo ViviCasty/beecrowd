8c2c1b8ff7aedab014ff0e6ed35c7ec21a2d34d0
ViviCasty <ichpendragon@gmail.com>
2025-10-02 11:42:22 -0300

    Criação do projeto Desafio_1019 com lógica de tempo
    Adicionado o arquivo de solução `Desafio_1019.sln` com configurações do Visual Studio e informações do projeto.
    
    Incluído o arquivo de projeto `Desafio_1019.csproj`, configurando o SDK do .NET, tipo de saída como executável e framework .NET 8.0, além de habilitar namespaces implícitos e verificação de nulidade.
    
    Implementada a lógica no arquivo `Program.cs` para calcular horas, minutos e segundos a partir de um total de segundos lido da entrada padrão, exibindo o resultado no formato "horas:minutos:segundos". Código comentado que utilizava `TimeSpan` foi removido.

---
+++ Desafio_1019/Desafio_1019/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        //int segundos = int.Parse(Console.ReadLine());
+
+        //TimeSpan ts = TimeSpan.FromSeconds(segundos);
+
+        //int horas = ts.Hours;
+        //int minutos = ts.Minutes;
+        //int segs = ts.Seconds;
+
+        //Console.WriteLine($"{horas}:{minutos}:{segs}");
+
+
+        int segundos = int.Parse(Console.ReadLine());
+
+        int horas = segundos / 3600;
+
+        int restoHoras = segundos % 3600;
+
+        int minutos = restoHoras / 60;
+
+        int segs = restoHoras % 60;
+
+        Console.WriteLine($"{horas}:{minutos}:{segs}");
+
+
+
+    }
+
+}
