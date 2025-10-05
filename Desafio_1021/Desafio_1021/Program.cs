30157da9f9608a90fbdf6e978d47d6265fe563c7
ViviCasty <ichpendragon@gmail.com>
2025-10-05 19:07:01 -0300

    Alterar tipo de valor para decimal e simplificar lógica
    O código foi atualizado para usar `decimal` em vez de `double` para representar valores monetários, melhorando a precisão. A lógica de cálculo das notas e moedas foi simplificada com o uso de arrays e um loop `foreach`, eliminando variáveis intermediárias. As saídas para notas e moedas agora são geradas dinamicamente, tornando o código mais conciso e legível.

--- Desafio_1021/Desafio_1021/Program.cs
+++ Desafio_1021/Desafio_1021/Program.cs
@@ -3,100 +3,28 @@ using System.Globalization;
 
 class URI
 {
-
     static void Main(string[] args)
     {
+        decimal valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        double nota100 = valor / 100.00;
-        int parteInteira100 = (int)nota100;
-
-        double resto100 = valor % 100;
-
-        double nota50 = resto100 / 50.00;
-
-        int parteInteira50 = (int)nota50;
-
-        double resto50 = resto100 % 50;
-
-        double nota20 = resto50 / 20.00;
-
-        int parteInteira20 = (int)nota20;
-
-        double resto20 = resto50 % 20;
-
-        double nota10 = resto20 / 10.00;
-
-        int parteInteira10 = (int)nota10;
-
-        double resto10 = resto20 % 10;
-
-        double nota5 = resto10 / 5.00;
-
-        int parteInteira5 = (int)nota5;
-
-        double resto5 = resto10 % 5;
-
-        double nota2 = resto5 / 2.00;
-
-        int parteInteira2 = (int)nota2;
-
-        double resto2 = resto5 % 2;
-
-
+        decimal[] notas = { 100.00m, 50.00m, 20.00m, 10.00m, 5.00m, 2.00m };
+        decimal[] moedas = { 1.00m, 0.50m, 0.25m, 0.10m, 0.05m, 0.01m };
 
         Console.WriteLine("NOTAS:");
-        Console.WriteLine(parteInteira100 + " nota(s) de R$ 100,00");
-        Console.WriteLine(parteInteira50 + " nota(s) de R$ 50,00");
-        Console.WriteLine(parteInteira20 + " nota(s) de R$ 20,00");
-        Console.WriteLine(parteInteira10 + " nota(s) de R$ 10,00");
-        Console.WriteLine(parteInteira5 + " nota(s) de R$ 5,00");
-        Console.WriteLine(parteInteira2 + " nota(s) de R$ 2,00");
-
-        double moeda1 = resto2 / 1.00;
-        int parteInteiraMoeda1 = (int)moeda1;
-        double restomoeda1 = moeda1 % 1.00;
-
-        double moedaCinquenta = restomoeda1 / 0.50;
-        int parteInteiraMoedaCinquenta = (int)moedaCinquenta;
-        double restoMoedaCinquenta = moedaCinquenta % 0.50;
-
-        double moeda25 = restoMoedaCinquenta / 0.25;
-        int partemoeda25 = (int)moeda25;
-        double restomoeda25 = moeda25 % 0.25;
-
-
-        double moeda10 = restomoeda25 / 0.10;
-        int partemoeda10 = (int)moeda10;
-        double restomoeda10 = moeda10 % 0.10;
-
-
-        double moeda05 = restomoeda10 / 0.05;
-        int partemoeda05 = (int)moeda05;
-        double restomoeda05 = moeda05 % 0.05;
-
-
-        double moeda01 = restomoeda05 / 0.01;
-        int partemoeda01 = (int)moeda01;
+        foreach (decimal nota in notas)
+        {
+            int quantidade = (int)(valor / nota);
+            Console.WriteLine($"{quantidade} nota(s) de R$ {nota:F2}");
+            valor -= quantidade * nota;
+        }
 
         Console.WriteLine("MOEDAS:");
-        Console.WriteLine(parteInteiraMoeda1 + " moeda(s) de R$ 1,00");
-        Console.WriteLine(parteInteiraMoedaCinquenta + " moeda(s) de R$ 0,50");
-        Console.WriteLine(partemoeda25 + " moeda(s) de R$ 0,25");
-        Console.WriteLine(partemoeda10 + " moeda(s) de R$ 0,10");
-        Console.WriteLine(partemoeda05 + " moeda(s) de R$ 0,05");
-        Console.WriteLine(partemoeda01 + " moeda(s) de R$ 0,01");
-
-
-
-
-
-
-
-
-
-
+        foreach (decimal moeda in moedas)
+        {
+            int quantidade = (int)(valor / moeda);
+            Console.WriteLine($"{quantidade} moeda(s) de R$ {moeda:F2}");
+            valor -= quantidade * moeda;
+        }
     }
-
 }
+
