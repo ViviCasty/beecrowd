615f3526b4e3d6e51964771e65b53f409ac242a9
ViviCasty <ichpendragon@gmail.com>
2025-10-06 23:45:23 -0300

    Metade da resolução da formula de bhaskara

---
+++ Desafio_1036/Desafio_1036/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double[] valores = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        double A = valores[0], B = valores[1], C = valores[2];
+
+        double delta = Math.Pow(B, 2) - 4 * A * C;
+
+        double r1 = (-B + Math.Sqrt(delta)) / (2 * A);
+        double r2 = (-B - Math.Sqrt(delta)) / (2 * A);
+
+        
+
+
+    }
+
+}
