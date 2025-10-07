86392a75dfc3adae35ff44d314e5195f2e9bd332
ViviCasty <ichpendragon@gmail.com>
2025-10-06 23:35:55 -0300

    Adicionar projeto Desafio_1035 com lógica de validação
    Criação do arquivo de solução `Desafio_1035.sln` e do arquivo de projeto `Desafio_1035.csproj`, configurando o uso do SDK do .NET 8.0.
    
    Adição do arquivo `Program.cs` com a classe `URI`, que implementa a lógica para ler e validar quatro inteiros da entrada padrão, imprimindo se os valores são aceitos ou não.

---
+++ Desafio_1035/Desafio_1035/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] entrada = Console.ReadLine().Split(' ');
+        int A = int.Parse(entrada[0]);
+        int B = int.Parse(entrada[1]);
+        int C = int.Parse(entrada[2]);
+        int D = int.Parse(entrada[3]);
+
+        if (B > C && D > A && (C + D) > (A+B) && C> 0 && D > 0 && A %2 ==0)
+        {
+            Console.WriteLine("Valores aceitos");
+        }
+        else
+        {
+            Console.WriteLine("Valores nao aceitos");
+        }
+
+
+    }
+
+}
