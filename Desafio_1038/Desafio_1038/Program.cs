268772b0cc4f2486a765033c0ced08f03081f13f
ViviCasty <ichpendragon@gmail.com>
2025-10-11 22:58:35 -0300

    Adicionar estrutura da solução e implementação do programa
    Criado o arquivo `Desafio_1038.sln` para definir a estrutura da solução do Visual Studio.
    Adicionado o arquivo `Desafio_1038.csproj` com configurações do projeto utilizando o SDK do .NET.
    Implementado o arquivo `Program.cs`, que lê um código de item e quantidade, calcula o total a partir de um dicionário de preços e exibe o resultado formatado.

---
+++ Desafio_1038/Desafio_1038/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+using System; 
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        Dictionary<int, double> ValorItem = new Dictionary<int, double>();
+
+        ValorItem.Add(1, 4.00); 
+        ValorItem.Add(2, 4.50);
+        ValorItem.Add(3, 5.00);
+        ValorItem.Add(4, 2.00);
+        ValorItem.Add(5, 1.50);
+
+        string[] valores = Console.ReadLine().Split(' ');
+
+        int codigo = int.Parse(valores[0]);
+        double quantidade = double.Parse(valores[1]);
+
+        if (ValorItem.ContainsKey(codigo))
+        {
+            double total = ValorItem[codigo] * quantidade;
+            Console.WriteLine($"Total: R$ {total.ToString("f2", CultureInfo.InvariantCulture)}");
+        }
+
+    }
+
+}
