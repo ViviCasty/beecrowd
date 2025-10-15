892abc36aa540e88b1be31414500155b9563a8d7
ViviCasty <ichpendragon@gmail.com>
2025-10-14 22:48:48 -0300

    Melhora a precisão e legibilidade do cálculo de média
    Substitui `Convert.ToDouble` por `double.Parse` para conversão de strings em números, utilizando `CultureInfo.InvariantCulture`. Atualiza a multiplicação dos pesos na média para `2.0`, `3.0` e `4.0` para garantir operações em ponto flutuante. Introduz a variável `mediaTruncada` e ajusta as condições de aprovação e reprovação para utilizá-la. Atualiza a exibição da média final para refletir a média truncada.

--- Desafio_1040/Desafio_1040/Program.cs
+++ Desafio_1040/Desafio_1040/Program.cs
@@ -1,52 +1,50 @@
-﻿using System;
+﻿
+using System;
 using System.Globalization;
 
 class URI
 {
-
     static void Main(string[] args)
     {
+        string[] notas = Console.ReadLine().Split(' ');
 
-        string[]  notas = Console.ReadLine().Split(' ');
+        double n1 = double.Parse(notas[0], CultureInfo.InvariantCulture);
+        double n2 = double.Parse(notas[1], CultureInfo.InvariantCulture);
+        double n3 = double.Parse(notas[2], CultureInfo.InvariantCulture);
+        double n4 = double.Parse(notas[3], CultureInfo.InvariantCulture);
 
-        double n1 = Convert.ToDouble(notas[0], CultureInfo.InvariantCulture);
-        double n2 = Convert.ToDouble(notas[1], CultureInfo.InvariantCulture);
-        double n3 = Convert.ToDouble(notas[2], CultureInfo.InvariantCulture);
-        double n4 = Convert.ToDouble(notas[3], CultureInfo.InvariantCulture);
+        double media = ((n1 * 2.0) + (n2 * 3.0) + (n3 * 4.0) + n4) / 10.0;
+        double mediaTruncada = Math.Floor(media * 10) / 10.0;
 
-        double media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + n4) / 10;
+        Console.WriteLine("Media: " + mediaTruncada.ToString("F1", CultureInfo.InvariantCulture));
 
-
-        if (media >= 7.0)
+        if (mediaTruncada >= 7.0)
         {
-            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
             Console.WriteLine("Aluno aprovado.");
         }
-        else if (media < 5.0)
+        else if (mediaTruncada < 5.0)
         {
-            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
             Console.WriteLine("Aluno reprovado.");
         }
-        else if (media >= 5.0 && media <= 6.9)
+        else
         {
-            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
             Console.WriteLine("Aluno em exame.");
-            double notaExame = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double notaExame = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Nota do exame: " + notaExame.ToString("F1", CultureInfo.InvariantCulture));
-            double mediaFinal = (media + notaExame) / 2;
-            if (mediaFinal >= 5.0)
+
+            double mediaFinal = (mediaTruncada + notaExame) / 2.0;
+            double mediaFinalTruncada = Math.Floor(mediaFinal * 10) / 10.0;
+
+            if (mediaFinalTruncada >= 5.0)
             {
                 Console.WriteLine("Aluno aprovado.");
-                Console.WriteLine("Media final: " + mediaFinal.ToString("F1", CultureInfo.InvariantCulture));
             }
             else
             {
                 Console.WriteLine("Aluno reprovado.");
-                Console.WriteLine("Media final: " + mediaFinal.ToString("F1", CultureInfo.InvariantCulture));
             }
+
+            Console.WriteLine("Media final: " + mediaFinalTruncada.ToString("F1", CultureInfo.InvariantCulture));
         }
-
-
     }
-
 }
